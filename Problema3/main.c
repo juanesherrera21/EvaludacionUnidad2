1e52ad2419e0ce398b13aa2c3fae464d9150f94f
HugoHernandez06 <hugoa.hernandez@upb.edu.co>
2021-09-21 23:11:54 -0700

    Sleep / Sin Hilos por ahora

--- Problema3/main.c
+++ Problema3/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
+
 
 int main(int argc, char *argv[]){
     for (int i = 0; i < argc; i++)
@@ -16,16 +18,16 @@ int main(int argc, char *argv[]){
 
     char buffer[64];
     char *status =  NULL;
-
+    status = fgets(buffer,sizeof(buffer),fin);
     do{
         status = fgets(buffer, sizeof(buffer),fin);
         if(status != NULL){
             printf("%s",buffer);
+            sleep(1);
         }
     }while (status !=NULL);
     printf("\n");
 
     fclose(fin);
-
     return(EXIT_SUCCESS);
 }
