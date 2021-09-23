667e1c95cae75cee48332515ea60d8c6538634b9
HugoHernandez06 <hugoa.hernandez@upb.edu.co>
2021-09-23 14:47:06 -0700

    Logica de comandos / Sin HILOS

--- Problema3/main.c
+++ Problema3/main.c
@@ -2,32 +2,78 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-
+#include <string.h>
+#include <ctype.h>
+#include <termios.h>
+#include "tlpi_hdr.h"
+#define BUF_SIZE 100
 
 int main(int argc, char *argv[]){
+    // Apagar el ECHO
+    if (tcgetattr(STDIN_FILENO, &tp) == -1)
+        errExit("tcgetattr");
+    save = tp;                          /* So we can restore settings later */
+    tp.c_lflag &= ~ECHO;                /* ECHO off, other bits unchanged */
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &tp) == -1)
+        errExit("tcsetattr");
+    // Lectura de argumentos argv    
     for (int i = 0; i < argc; i++)
     {
         printf("Argv[%d]: %s\n",i,argv[i]);
     }
-    
+    // Lectura del Archivo de Entrada 'fin'
     FILE *fin = fopen(argv[1],"r+");
     if (fin == NULL){
         perror("open fin file fails: ");
         return(EXIT_FAILURE);
     }
-
     char buffer[64];
-    char *status =  NULL;
-    status = fgets(buffer,sizeof(buffer),fin);
+    char *status;
     do{
         status = fgets(buffer, sizeof(buffer),fin);
         if(status != NULL){
             printf("%s",buffer);
-            sleep(1);
+            //sleep(1);
         }
     }while (status !=NULL);
     printf("\n");
-
+    
+    // Comando 'upper'
+    char comando[10];
+    scanf("%s",comando);
+    if((strcmp(comando,"upper"))==0){
+        for (int i = 0; buffer[i] != '\0'; ++i){
+		    buffer[i] = toupper(buffer[i]);
+	    }
+	    printf("upper: %s\n", buffer);
+    }
+    
+    // Comando 'words'
+    int count = 0;
+    char lastC;
+    int len  = strlen(buffer);
+    if((strcmp(comando,"words"))==0){
+        for (int i = 0; i <= len; i++) {
+            if((buffer[i]==' ' || buffer[i]=='\0') && lastC != ' '){
+                count++;
+            }
+            lastC = buffer[i];
+        }
+        printf("words: %d\n",count);
+    }
+    
+    // comando 'vowels'
+    if((strcmp(comando,"vowels"))==0){
+        int vowels = 0;
+        for (int i = 0; buffer[i] != '\0'; i++) {
+            char lastL = tolower(buffer[i]);
+            if(lastL == 'a' || lastL == 'e' || lastL == 'i' || lastL == 'o' || lastL == 'u'){
+                vowels++;
+            }
+        }
+        printf("vowels: %d\n",vowels);
+    }
+    
     fclose(fin);
     return(EXIT_SUCCESS);
 }
