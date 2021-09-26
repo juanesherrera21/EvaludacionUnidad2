19701b3f14cbdb0e1118792dec4fc397cbfeedf8
juanesherrera21 <68380237+juanesherrera21@users.noreply.github.com>
2021-09-26 10:25:46 -0500

    Solucion al problema 1
    Se compartio la solucion al problema 1, el codigo fue implementado en C#

---
+++ problema1/problema1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            intarray parr = new intarray(100);
+            Program obj = new Program();
+            obj.initIntArray(parr);
+            obj.printArray(parr);
+            //GC.Collect();
+            //GC.WaitForPendingFinalizers();
+            Console.ReadKey();
+        }
+
+        public void initIntArray(intarray v1) {
+           
+            for (int i = 0; i < v1.Leght; i++)
+            {
+                v1.Arr[i] = i;
+            }
+
+        }
+
+        public void printArray(intarray v1)
+        {
+            for (int i = 0; i < v1.Leght; i++)
+            {
+                Console.WriteLine("parr[" + i + "]" + ":" + v1.Arr[i]);
+            }
+        }
+
+    }
+}
---
+++ problema1/problema1/intarray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema1
+{
+    class intarray
+    {
+        //atribute:
+        private int[] arr;
+        private int leght;
+
+        public int[] Arr { get => arr; set => arr = value; }
+        public int Leght { get => leght; set => leght = value; }
+
+
+        //methodos:
+        public intarray(int leght) {
+            Arr = new int[leght];
+            this.Leght = leght;
+        }
+
+    }
+}
